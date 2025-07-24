4d97849fb6becedb30b95da4d69bca1e17aa7858
yjc9696 <616530803@qq.com>
2025-07-24 18:03:12 +0800

    remove test

--- tests/models/hunyuan_v1_dense/test_modeling_hunyuan_v1_dense.py
+++ tests/models/hunyuan_v1_dense/test_modeling_hunyuan_v1_dense.py
@@ -34,60 +34,60 @@ if is_torch_available():
 from ...causal_lm_tester import CausalLMModelTest, CausalLMModelTester
 
 
-class HunYuanDenseV1ModelTester(CausalLMModelTester):
-    config_class = HunYuanDenseV1Config
-    if is_torch_available():
-        base_model_class = HunYuanDenseV1Model
-        causal_lm_class = HunYuanDenseV1ForCausalLM
-        sequence_class = HunYuanDenseV1ForSequenceClassification
+# class HunYuanDenseV1ModelTester(CausalLMModelTester):
+#     config_class = HunYuanDenseV1Config
+#     if is_torch_available():
+#         base_model_class = HunYuanDenseV1Model
+#         causal_lm_class = HunYuanDenseV1ForCausalLM
+#         sequence_class = HunYuanDenseV1ForSequenceClassification
 
 
-@require_torch
-class HunYuanDenseV1ModelTest(CausalLMModelTest, unittest.TestCase):
-    all_model_classes = (
-        (
-            HunYuanDenseV1Model,
-            HunYuanDenseV1ForCausalLM,
-            HunYuanDenseV1ForSequenceClassification,
-        )
-        if is_torch_available()
-        else ()
-    )
-    test_headmasking = False
-    test_pruning = False
-    model_tester_class = HunYuanDenseV1ModelTester
-    pipeline_model_mapping = (
-        {
-            "feature-extraction": HunYuanDenseV1Model,
-            "text-generation": HunYuanDenseV1ForCausalLM,
-            "text-classification": HunYuanDenseV1ForSequenceClassification,
-        }
-        if is_torch_available()
-        else {}
-    )
+# @require_torch
+# class HunYuanDenseV1ModelTest(CausalLMModelTest, unittest.TestCase):
+#     all_model_classes = (
+#         (
+#             HunYuanDenseV1Model,
+#             HunYuanDenseV1ForCausalLM,
+#             HunYuanDenseV1ForSequenceClassification,
+#         )
+#         if is_torch_available()
+#         else ()
+#     )
+#     test_headmasking = False
+#     test_pruning = False
+#     model_tester_class = HunYuanDenseV1ModelTester
+#     pipeline_model_mapping = (
+#         {
+#             "feature-extraction": HunYuanDenseV1Model,
+#             "text-generation": HunYuanDenseV1ForCausalLM,
+#             "text-classification": HunYuanDenseV1ForSequenceClassification,
+#         }
+#         if is_torch_available()
+#         else {}
+#     )
 
-    def is_pipeline_test_to_skip(
-        self,
-        pipeline_test_case_name,
-        config_class,
-        model_architecture,
-        tokenizer_name,
-        image_processor_name,
-        feature_extractor_name,
-        processor_name,
-    ):
-        return True
+#     def is_pipeline_test_to_skip(
+#         self,
+#         pipeline_test_case_name,
+#         config_class,
+#         model_architecture,
+#         tokenizer_name,
+#         image_processor_name,
+#         feature_extractor_name,
+#         processor_name,
+#     ):
+#         return True
 
 
-@require_torch
-class HunYuanDenseV1IntegrationTest(unittest.TestCase):
-    def setUp(self):
-        cleanup(torch_device, gc_collect=True)
+# @require_torch
+# class HunYuanDenseV1IntegrationTest(unittest.TestCase):
+#     def setUp(self):
+#         cleanup(torch_device, gc_collect=True)
 
-    def tearDown(self):
-        cleanup(torch_device, gc_collect=True)
+#     def tearDown(self):
+#         cleanup(torch_device, gc_collect=True)
 
-    @slow
-    def test_model_generation(self):
-        # TODO Need new Dense Model
-        return True
+#     @slow
+#     def test_model_generation(self):
+#         # TODO Need new Dense Model
+#         return True
--- tests/models/hunyuan_v1_moe/test_modeling_hunyuan_v1_moe.py
+++ tests/models/hunyuan_v1_moe/test_modeling_hunyuan_v1_moe.py
@@ -34,76 +34,76 @@ if is_torch_available():
 from ...causal_lm_tester import CausalLMModelTest, CausalLMModelTester
 
 
-class HunYuanMoEV1ModelTester(CausalLMModelTester):
-    config_class = HunYuanMoeV1Config
-    if is_torch_available():
-        base_model_class = HunYuanMoEV1Model
-        causal_lm_class = HunYuanMoEV1ForCausalLM
-        sequence_class = HunYuanMoEV1ForSequenceClassification
+# class HunYuanMoEV1ModelTester(CausalLMModelTester):
+#     config_class = HunYuanMoeV1Config
+#     if is_torch_available():
+#         base_model_class = HunYuanMoEV1Model
+#         causal_lm_class = HunYuanMoEV1ForCausalLM
+#         sequence_class = HunYuanMoEV1ForSequenceClassification
 
 
-@require_torch
-class HunYuanMoEV1ModelTest(CausalLMModelTest, unittest.TestCase):
-    all_model_classes = (
-        (
-            HunYuanMoEV1Model,
-            HunYuanMoEV1ForCausalLM,
-            HunYuanMoEV1ForSequenceClassification,
-        )
-        if is_torch_available()
-        else ()
-    )
-    test_headmasking = False
-    test_pruning = False
-    model_tester_class = HunYuanMoEV1ModelTester
-    pipeline_model_mapping = (
-        {
-            "feature-extraction": HunYuanMoEV1Model,
-            "text-generation": HunYuanMoEV1ForCausalLM,
-            "text-classification": HunYuanMoEV1ForSequenceClassification,
-        }
-        if is_torch_available()
-        else {}
-    )
+# @require_torch
+# class HunYuanMoEV1ModelTest(CausalLMModelTest, unittest.TestCase):
+#     all_model_classes = (
+#         (
+#             HunYuanMoEV1Model,
+#             HunYuanMoEV1ForCausalLM,
+#             HunYuanMoEV1ForSequenceClassification,
+#         )
+#         if is_torch_available()
+#         else ()
+#     )
+#     test_headmasking = False
+#     test_pruning = False
+#     model_tester_class = HunYuanMoEV1ModelTester
+#     pipeline_model_mapping = (
+#         {
+#             "feature-extraction": HunYuanMoEV1Model,
+#             "text-generation": HunYuanMoEV1ForCausalLM,
+#             "text-classification": HunYuanMoEV1ForSequenceClassification,
+#         }
+#         if is_torch_available()
+#         else {}
+#     )
 
-    def is_pipeline_test_to_skip(
-        self,
-        pipeline_test_case_name,
-        config_class,
-        model_architecture,
-        tokenizer_name,
-        image_processor_name,
-        feature_extractor_name,
-        processor_name,
-    ):
-        return True
+#     def is_pipeline_test_to_skip(
+#         self,
+#         pipeline_test_case_name,
+#         config_class,
+#         model_architecture,
+#         tokenizer_name,
+#         image_processor_name,
+#         feature_extractor_name,
+#         processor_name,
+#     ):
+#         return True
 
 
-@require_torch
-class HunYuanMoEV1IntegrationTest(unittest.TestCase):
-    def setUp(self):
-        cleanup(torch_device, gc_collect=True)
+# @require_torch
+# class HunYuanMoEV1IntegrationTest(unittest.TestCase):
+#     def setUp(self):
+#         cleanup(torch_device, gc_collect=True)
 
-    def tearDown(self):
-        cleanup(torch_device, gc_collect=True)
+#     def tearDown(self):
+#         cleanup(torch_device, gc_collect=True)
 
-    @slow
-    def test_model_generation(self):
-        EXPECTED_ANSWER = "\nRegular exercise offers numerous physical, mental, and emotional benefits. It improves cardiovascular health, strengthens muscles and bones, boosts metabolism, and helps"
-        prompt = "Write a short summary of the benefits of regular exercise "
-        tokenizer = AutoTokenizer.from_pretrained("tencent/Hunyuan-A13B-Instruct", use_fast=False)
-        model = HunYuanMoEV1ForCausalLM.from_pretrained("tencent/Hunyuan-A13B-Instruct", device_map="auto")
-        messages = [
-            {"role": "user", "content": prompt},
-        ]
-        tokenized_chat = tokenizer.apply_chat_template(
-            messages,
-            tokenize=True,
-            add_generation_prompt=True,
-            return_tensors="pt",
-            enable_thinking=False,  # Toggle thinking mode (default: True)
-        )
-        generated_ids = model.generate(tokenized_chat.to(model.device), max_new_tokens=30, top_k=1)
-        text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-        answer = text.split("<answer>")[1]
-        self.assertEqual(EXPECTED_ANSWER, answer)
+#     @slow
+#     def test_model_generation(self):
+#         EXPECTED_ANSWER = "\nRegular exercise offers numerous physical, mental, and emotional benefits. It improves cardiovascular health, strengthens muscles and bones, boosts metabolism, and helps"
+#         prompt = "Write a short summary of the benefits of regular exercise "
+#         tokenizer = AutoTokenizer.from_pretrained("tencent/Hunyuan-A13B-Instruct", use_fast=False)
+#         model = HunYuanMoEV1ForCausalLM.from_pretrained("tencent/Hunyuan-A13B-Instruct", device_map="auto")
+#         messages = [
+#             {"role": "user", "content": prompt},
+#         ]
+#         tokenized_chat = tokenizer.apply_chat_template(
+#             messages,
+#             tokenize=True,
+#             add_generation_prompt=True,
+#             return_tensors="pt",
+#             enable_thinking=False,  # Toggle thinking mode (default: True)
+#         )
+#         generated_ids = model.generate(tokenized_chat.to(model.device), max_new_tokens=30, top_k=1)
+#         text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+#         answer = text.split("<answer>")[1]
+#         self.assertEqual(EXPECTED_ANSWER, answer)

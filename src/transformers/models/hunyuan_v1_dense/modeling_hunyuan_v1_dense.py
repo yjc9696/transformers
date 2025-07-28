9e202b24a775babff6a363d706b411b045154149
Mingji Han <mingjihan@tencent.com>
2025-07-28 15:31:32 +0800

    fix format

--- src/transformers/models/hunyuan_v1_dense/modeling_hunyuan_v1_dense.py
+++ src/transformers/models/hunyuan_v1_dense/modeling_hunyuan_v1_dense.py
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
-from ...utils import TransformersKwargs
+
 from transformers.activations import ACT2FN
 from transformers.cache_utils import Cache, DynamicCache
 from transformers.modeling_attn_mask_utils import (
@@ -37,8 +37,10 @@ from transformers.utils import (
     replace_return_docstrings,
 )
 from transformers.utils.import_utils import is_torch_fx_available
-from ...processing_utils import Unpack
+
 from ...generation import GenerationMixin
+from ...processing_utils import Unpack
+from ...utils import TransformersKwargs
 from .configuration_hunyuan_v1_dense import HunYuanDenseV1Config
 
 
@@ -1329,7 +1331,7 @@ class HunYuanDenseV1ForCausalLM(HunYuanDenseV1PreTrainedModel, GenerationMixin):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
-        **super_kwargs: Unpack[TransformersKwargs]
+        **super_kwargs: Unpack[TransformersKwargs],
     ) -> Union[tuple, CausalLMOutputWithPast]:
         r"""
         Args:

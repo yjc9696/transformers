6c8dc58ad6cc4d796d3bf48a255d2b61daf269a7
yjc9696 <616530803@qq.com>
2025-08-06 12:13:30 +0800

    fix

--- src/transformers/models/hunyuan_v1_dense/modeling_hunyuan_v1_dense.py
+++ src/transformers/models/hunyuan_v1_dense/modeling_hunyuan_v1_dense.py
@@ -192,8 +192,8 @@ class HunYuanDenseV1Attention(nn.Module):
         input_shape = hidden_states.shape[:-1]
         hidden_shape = (*input_shape, -1, self.head_dim)
 
-        query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
-        key_states = self.k_proj(hidden_states).view(hidden_shape).transpose(1, 2)
+        query_states = self.q_norm(self.q_proj(hidden_states).view(hidden_shape)).transpose(1, 2)
+        key_states = self.k_norm(self.k_proj(hidden_states).view(hidden_shape)).transpose(1, 2)
         value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
 
         cos, sin = position_embeddings
